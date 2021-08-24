444b99e69049dde4930eee85e3013779ed59d631
Kanye09 <kanybek09@yahoo.com>
2021-08-24 00:44:02 -0400

    updating stripe integratin, favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JRTl0J2N0m9kFIJXccUG5UDFzAsq399RGXnEZefV3JGE93swlL6qqIMMslI2BlHMHBpUmctw33REruyncah31gd00RdPThBh8";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
