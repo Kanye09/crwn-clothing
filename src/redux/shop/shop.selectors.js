fbb04ca98d6b79aaafc37cebae1cba12dda60b75
Kanye09 <kanybek09@yahoo.com>
2021-09-08 19:50:36 -0400

    created user and cart sagasfor sign in, sign up and sign in with google

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,7 +1,5 @@
 import { createSelector } from "reselect";
 
-import memoize from "lodash.memoize";
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -15,8 +13,17 @@ export const selectCollectionsForPreview = createSelector(
     collections ? Object.keys(collections).map((key) => collections[key]) : []
 );
 
-export const selectCollection = memoize((collectionUrlParam) =>
+export const selectCollection = (collectionUrlParam) =>
   createSelector([selectCollections], (collections) =>
     collections ? collections[collectionUrlParam] : null
-  )
+  );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  (shop) => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  (shop) => !!shop.collections
 );
